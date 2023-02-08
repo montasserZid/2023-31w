5832eac0fb8e550d2bc6fd76fd241c3017e976fc
Montasser <montasser.zid19@gmail.com>
2023-02-08 10:57:10 -0500

    bloginfo V2

--- header.php
+++ header.php
@@ -9,5 +9,7 @@
 </head>
 <body>
     <head>
-        <h1><?php bloginfo(); ?>
+        <h1><?php echo '1'; bloginfo('name'); ?></h1>
+        <h2><?php echo '2'; bloginfo('description'); ?></h2>
+        <h2><?php echo '3'; bloginfo('url'); ?></h2>
     </head>
