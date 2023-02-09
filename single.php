f6230964916dc21322bcf74a8adb63f2ce5f9f8d
Montasser <montasser.zid19@gmail.com>
2023-02-09 16:16:14 -0500

    the_permalink()

--- single.php
+++ single.php
@@ -7,15 +7,14 @@ get_header() ?>
     <h3>index.php</h3>
 <?php 
 if (have_posts()):
-    while (have_posts()) : the_post();
+    
+   while (have_posts()) : the_post();
+
         the_title('<h1>','</h1>');
-        the_permalink();
+        the_content();
         // the_excerpt();
-        ?>
-        <h1><a href="<?= the_permalink(); ?>"><?= get_the_title(); ?></a></h1>
-            <?php
-       echo wp_trim_words(get_the_excerpt(), 4);
-    endwhile;
+       
+   endwhile;
 endif;
 ?>   
 </main> 
