e2b3c74b0413a7a9721285284b44456822209f07
Montasser <montasser.zid19@gmail.com>
2023-03-16 14:58:46 -0400

    c14 menu entete

--- functions.php
+++ functions.php
@@ -8,6 +8,45 @@ function ajouter_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
 
+/* ------------------------------------------------- Enregistrement des menus */
+
+function enregistrement_des_menus(){
+    register_nav_menus( array(
+        'menu_entete' => 'Menu entête' ,
+        'menu_footer'  => 'Menu pied de page',
+    ) );
+}
+add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
+
+/**
+ * Modifie la requete principale de Wordpress avant qu'elle soit exécuté
+ * le hook « pre_get_posts » se manifeste juste avant d'exécuter la requête principal
+ * Dépendant de la condition initiale on peut filtrer un type particulier de requête
+ * Dans ce cas çi nous filtrons la requête de la page d'accueil
+ * @param WP_query  $query la requête principal de WP
+ */
+function cidweb_modifie_requete_principal( $query ) {
+    if (    $query->is_home() // si page d'accueil
+            && $query->is_main_query() // si requête principale
+            && ! is_admin() ) { // non tableau de bord
+      $query->set( 'category_name', 'note-wp' ); // filtre les articles de categorie «note-wp»
+      $query->set( 'orderby', 'title' );// trie selon le titre
+      $query->set( 'order', 'ASC' ); // en ordre ascendant
+      }
+     }
+     add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
+
+/**
+ * Permet de modifier les titre du menu « cours »
+ * @param $title : titre du choix menu
+ * @param $item : le choix global
+ * @param $args: Object qui représente la structure de menu 
+ * 
+ */
+
+
+
+
 /* --------------------------------------------------- Add_theme_support */
 add_theme_support( 'html5', 
                     array(  'search-form',
@@ -23,53 +62,16 @@ add_theme_support( 'custom-logo',
                         'width'  => 150,
                     ) );
 
-/* ------------------------------------------------- Enregistrement des menus */
-
-    function enregistrement_des_menus(){
-        register_nav_menus( array(
-            'menu_entete' => 'Menu entête' ,
-            'menu_footer'  => 'Menu pied de page',
-        ) );
-    }
-    add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
-
-    /**
- * Modifie la requete principale de Wordpress avant qu'elle soit exécuté
- * le hook « pre_get_posts » se manifeste juste avant d'exécuter la requête principal
- * Dépendant de la condition initiale on peut filtrer un type particulier de requête
- * Dans ce cas çi nous filtrons la requête de la page d'accueil
- * @param WP_query  $query la requête principal de WP
- */
-
-
-function cidweb_modifie_requete_principal( $query ) {
-    if ( $query->is_home()  //si pafe d'accueil
-    && $query->is_main_query() //si requête principale
-    && ! is_admin() ) {     //si pas dans l'admin
-      $query->set( 'category_name', 'note-wordpress' ); // on filtre par catégorie << note-wp >>
-      $query->set( 'orderby', 'title' );        // on trie par titre
-      $query->set( 'order', 'ASC' );           // dans l'ordre croissant
-      }
-     }
-
-     add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );  
- /**
-  *   permet de modifier les titres de menu cours
-  * @param $title : titre du choix de menu
-    * @param $item : le choix global du menu
-    * @param $args : les arguments du menu
-   */     
-  function perso_menu_item_title($title, $item, $args) {
-    // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu
+function perso_menu_item_title($title, $item, $args) {
+    // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu  
+  
     if($args->menu == 'cours') {
-        // Modifier la longueur du titre en fonction de vos besoins
-        $sigle = substr($title, 4, 3);
-        $title = substr($title, 7);
-        $title = "<div class='cours__sigle'>" . $sigle . "</div>" . 
-        "<p class='cours__titre'>" .  $title ."</p>";
+    // Modifier la longueur du titre en fonction de vos besoins
+    $sigle = substr($title, 4,3);
+    $title = substr($title, 7);
+    $title = "<div class='cours__sigle'>" . $sigle . "</div>" . 
+              "<p class='cours__titre'>" . wp_trim_words($title, 2, ' ... ') . "</p>";
     }
-    return $title;
+    return  $title ;
 }
 add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
-
-    
