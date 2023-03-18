fb8a34e61ee1c3d3c949a221be03491ef78c933c
Montasser <montasser.zid19@gmail.com>
2023-03-18 14:04:08 -0400

    c14 ajustement footer.php+style

--- footer.php
+++ footer.php
@@ -3,8 +3,12 @@
 * Template footer.php
 */
 ?>
-<footer class="sitye__footer">
-<h3>Pied de page</h3>
+<footer class="site__footer">
+  <h3>Pied de page</h3>
+  <ul>
+    <li><a href="https://github.com/montasserZid/2023-31w/tree/tp1">TP1 sur GitHub</a></li>
+    <li><a href="https://github.com/montasserZid/carrousel-31w">Caroussel sur GitHub</a></li>
+  </ul>
 </footer>
 <?php wp_footer(); ?>
 </body>
