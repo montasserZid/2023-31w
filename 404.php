d66d8bedc769cc32dcb12e0f4062e6a5f732c3c2
Montasser <montasser.zid19@gmail.com>
2023-03-30 16:46:18 -0400

    c18 re-modification _menu_bloc + viewport

--- 404.php
+++ 404.php
@@ -2,11 +2,12 @@
 /**
     Modèle index.php représente le modèle par défaut du thème
 */
-get_header() ?>
+get_header();
+?>
 <main class="site__main">
-    <code>404.php</code>
-    <h1>Erreur 404 - la page n'existe pas</h1>
-</main> 
+    <div class="error-404">
+        <img src="<?php echo esc_url(get_template_directory_uri() . '/images/404-error.png'); ?>" alt="Erreur 404">
+        <h1>Erreur 404 - la page n'existe pas</h1>
+    </div>
+</main>
 <?php get_footer(); ?>
-
-<!-- i7eb des modification houni  -->
