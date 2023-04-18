bbd2a2b849a24e4e68551cbedc18e28fd92aeec0
Montasser <montasser.zid19@gmail.com>
2023-04-18 14:55:08 -0400

    examfinal - ajustement bar-recherche dans erreur 404

--- 404.php
+++ 404.php
@@ -9,7 +9,9 @@ get_header();
         <img src="<?php echo esc_url(get_template_directory_uri() . '/images/404-error.png'); ?>" alt="Erreur 404">
         <h1>Erreur 404</h1>
         <h2>Page introuvable , vouz pouvez tenter une recherche</h2>
+        <div class="erreur-componant">
         <?= get_search_form(); ?>
+        </div>
     </div>
 </main>
 <?php get_footer(); ?>
