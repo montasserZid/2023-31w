3bd6b3c15d85cd30aab641311d86de0823147304
Montasser <montasser.zid19@gmail.com>
2023-04-18 14:19:22 -0400

    exam final test commit et push

--- functions.php
+++ functions.php
@@ -87,25 +87,9 @@ function perso_menu_item_title($title, $item, $args) {
     }
     return  $title ;
 }
+//test
 add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
-/////////////////////////////////////////// enregistrement d'un sidebar/
-
-function ajouter_description_class_menu( $items, $args ) {
-    // Vérifier si le menu correspondant est celui que vous souhaitez modifier
-    if ( 'evenement' === $args->menu ) {
-        foreach ( $items as $item ) {
-            // Récupérer le titre, la description et la classe personnalisée
-            $titre = $item->title;
-            $description = $item->description;
-            $classe = 'nom_de_la_classe'; // Remplacer par le nom de la classe souhaitée
-            // Ajouter la description et la classe personnalisée à l'élément de menu
-            $item->title .= '<span class="' . $classe . '">' . $description . '</span>';
-        }
-    }
-    return $items;
-}
-add_filter( 'wp_nav_menu_objects', 'ajouter_description_class_menu', 10, 2 );
-
+/////////////////////////////////////////// enregistrement d'un sidebar
 
 // Enregistrer le sidebar
 function enregistrer_sidebar() {
