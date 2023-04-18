60b02b14ee37f03846d91b2f6287bcf779d09b49
Montasser <montasser.zid19@gmail.com>
2023-04-18 14:37:52 -0400

    examfinal - ajustement titre et sous titre erreur 404

--- 404.php
+++ 404.php
@@ -7,7 +7,8 @@ get_header();
 <main class="site__main">
     <div class="error-404">
         <img src="<?php echo esc_url(get_template_directory_uri() . '/images/404-error.png'); ?>" alt="Erreur 404">
-        <h1>Erreur 404 - la page n'existe pas</h1>
+        <h1>Erreur 404</h1>
+        <h2>Page introuvable , vouz pouvez tenter une recherche</h2>
     </div>
 </main>
 <?php get_footer(); ?>
